7a92dc7b85c7b0fef5279ea137ab29c9a2c020f0
zhaisabrina <szhai@andrew.cmu.edu>
2020-12-11 16:03:29 -0500

    hopefully this works
    redeployed and moved build to docs

---
+++ docs/precache-manifest.c2a423b778ee0e60eb6ade762080dbe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d52a2e449d82f95b871dcfc3e6b8169",
+    "url": "/todoapp.git/index.html"
+  },
+  {
+    "revision": "8dd8dd2a768ad889d5a9",
+    "url": "/todoapp.git/static/css/2.fc88e6b0.chunk.css"
+  },
+  {
+    "revision": "1312560be6005c0667cf",
+    "url": "/todoapp.git/static/css/main.18e1dd05.chunk.css"
+  },
+  {
+    "revision": "8dd8dd2a768ad889d5a9",
+    "url": "/todoapp.git/static/js/2.cc9ea667.chunk.js"
+  },
+  {
+    "revision": "1312560be6005c0667cf",
+    "url": "/todoapp.git/static/js/main.3e86a2a4.chunk.js"
+  },
+  {
+    "revision": "8b2bf2e1ab9a83e4552d",
+    "url": "/todoapp.git/static/js/runtime~main.5ee33480.js"
+  }
+]);
